f77bfba5c6004e0ee84b61f7dbc314aa08477b54
Orlando Watson <ogw@orlandogwatson.com>
2018-02-07 09:49:55 -0800

    gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,18 @@
-const postcss = require('gulp-postcss');
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
-const notify = require('gulp-notify');
-const livereload = require('gulp-livereload');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', () => {
-  livereload.listen();
-
   nodemon({
     script: './bin/start.js',
     ext: 'js scss'
   }).on('restart', () => {
     gulp.src('./public/stylesheets/*.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./public/stylesheets'))
-      .pipe(postcss())
+      .pipe(autoprefixer())
       .pipe(gulp.dest('./public/stylesheets'));
-
-    gulp.src('./bin/start.js')
-      .pipe(livereload())
-      .pipe(notify('Reloading page, please wait...'));
   });
 });
+
+gulp.watch('default');
