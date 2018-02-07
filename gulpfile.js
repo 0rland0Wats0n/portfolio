432f010ae2fb51630618217b431571b17e66916f
Orlando Watson <ogw@orlandogwatson.com>
2018-02-07 10:10:42 -0800

    fix sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,22 @@ const nodemon = require('gulp-nodemon');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', () => {
-  nodemon({
-    script: './bin/start.js',
-    ext: 'js scss'
-  }).on('restart', () => {
-    console.log('Restarting....');
-  });
-});
-
 gulp.task('sass', () => {
-  gulp.src('./public/stylesheets/*.scss')
+  return gulp.src('./public/stylesheets/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest('./public/stylesheets'));
 });
 
-gulp.watch('default', ['sass']);
+gulp.task('watch', () => {
+  gulp.watch('sass');
+});
+
+gulp.task('default', ['watch'], () => {
+  return nodemon({
+    script: './bin/start.js',
+    ext: 'js scss'
+  }).on('restart', () => {
+    console.log('restarted');
+  });
+});
